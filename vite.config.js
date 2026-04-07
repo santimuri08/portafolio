468cfc0cc5902ae0b889cfebd606a6c955eaf019
santimuri08 <115190223+santimuri08@users.noreply.github.com>
2026-04-06 23:59:27 -0400

    Enhance Vite config with modulePreload and minify options
    Added modulePreload and minification options to build configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -13,6 +12,17 @@ export default defineConfig({
           'lucide': ['lucide-react'],
         }
       }
-    }
-  }
+    },
+    modulePreload: {
+      polyfill: true,
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log'],
+      },
+    },
+  },
 })
